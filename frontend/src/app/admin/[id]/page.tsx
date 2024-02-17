00a5ec157e04af84190d3e0b11441eebd3b0c338
Errieka Palanca <errieka.palanca@gmail.com>
2024-02-17 14:37:10 +0800

    feat: added navigation menu for admin

--- frontend/src/app/admin/[id]/page.tsx
+++ frontend/src/app/admin/[id]/page.tsx
@@ -1,5 +1,41 @@
 "use client";
+import "./styles.css";
+import Navbar from "@/app/navbar/page";
+import NavigationMenuDemo from "@/app/navigationmenu/page";
+import { NavigationMenu } from "@radix-ui/react-navigation-menu";
+
+import { Card, Container, Flex, Inset, Strong, Text } from "@radix-ui/themes";
 
 export default function Page() {
-  return <h1>ADMIN</h1>;
+  return (
+    <main>
+      <NavigationMenuDemo></NavigationMenuDemo>
+      <Container>
+        <Flex className="CardContainer">
+          <Card className="Card">
+            <a href="/">
+              <Inset clip="padding-box" side="top" pb="current"></Inset>
+              <Text as="p" align="center" className="Text">
+                <Strong>Patients in Queue</Strong>
+              </Text>
+            </a>
+          </Card>
+          <Card className="Card">
+            <a href="/">
+              <Text as="p" align="center" className="Text">
+                <Strong>Doctors on Duty</Strong>
+              </Text>
+            </a>
+          </Card>
+          <Card className="Card">
+            <a href="/">
+              <Text as="p" align="center" className="Text">
+                <Strong>Appointments Today</Strong>
+              </Text>
+            </a>
+          </Card>
+        </Flex>
+      </Container>
+    </main>
+  );
 }
---
+++ frontend/src/app/navigationmenu/page.tsx
@@ -0,0 +1,163 @@
+import React from "react";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import classNames from "classnames";
+import { CaretDownIcon } from "@radix-ui/react-icons";
+import "./styles.css";
+import * as Avatar from "@radix-ui/react-avatar";
+
+const NavigationMenuDemo = () => {
+  return (
+    <div className="CenteredContainer">
+      <NavigationMenu.Root className="NavigationMenuRoot">
+        <NavigationMenu.List className="NavigationMenuList">
+          <NavigationMenu.Item className="NavigationMenuItem">
+            <Avatar.Root className="AvatarRoot">
+              <Avatar.Image
+                className="AvatarImage"
+                src="https://www.addu.edu.ph/wp-content/uploads/2020/08/UniversitySeal480px.png"
+                alt="Colm Tuite"
+              />
+              <Avatar.Fallback className="AvatarFallback" delayMs={600}>
+                CT
+              </Avatar.Fallback>
+            </Avatar.Root>
+          </NavigationMenu.Item>
+          <NavigationMenu.Item className="NavigationMenuItem">
+            <NavigationMenu.Trigger className="NavigationMenuTrigger">
+              Patient <CaretDownIcon className="CaretDown" aria-hidden />
+            </NavigationMenu.Trigger>
+            <NavigationMenu.Content className="NavigationMenuContent">
+              <ul className="List one">
+                <li style={{ gridRow: "span 3" }}>
+                  <NavigationMenu.Link asChild>
+                    <a className="Callout" href="/">
+                      <svg
+                        width="25"
+                        height="25"
+                        viewBox="0 0 15 15"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M0.877014 7.49988C0.877014 3.84219 3.84216 0.877045 7.49985 0.877045C11.1575 0.877045 14.1227 3.84219 14.1227 7.49988C14.1227 11.1575 11.1575 14.1227 7.49985 14.1227C3.84216 14.1227 0.877014 11.1575 0.877014 7.49988ZM7.49985 1.82704C4.36683 1.82704 1.82701 4.36686 1.82701 7.49988C1.82701 8.97196 2.38774 10.3131 3.30727 11.3213C4.19074 9.94119 5.73818 9.02499 7.50023 9.02499C9.26206 9.02499 10.8093 9.94097 11.6929 11.3208C12.6121 10.3127 13.1727 8.97172 13.1727 7.49988C13.1727 4.36686 10.6328 1.82704 7.49985 1.82704ZM10.9818 11.9787C10.2839 10.7795 8.9857 9.97499 7.50023 9.97499C6.01458 9.97499 4.71624 10.7797 4.01845 11.9791C4.97952 12.7272 6.18765 13.1727 7.49985 13.1727C8.81227 13.1727 10.0206 12.727 10.9818 11.9787ZM5.14999 6.50487C5.14999 5.207 6.20212 4.15487 7.49999 4.15487C8.79786 4.15487 9.84999 5.207 9.84999 6.50487C9.84999 7.80274 8.79786 8.85487 7.49999 8.85487C6.20212 8.85487 5.14999 7.80274 5.14999 6.50487ZM7.49999 5.10487C6.72679 5.10487 6.09999 5.73167 6.09999 6.50487C6.09999 7.27807 6.72679 7.90487 7.49999 7.90487C8.27319 7.90487 8.89999 7.27807 8.89999 6.50487C8.89999 5.73167 8.27319 5.10487 7.49999 5.10487Z"
+                          fill="white"
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+
+                      <div className="CalloutHeading">
+                        View Patient <br />
+                        Profile
+                      </div>
+                    </a>
+                  </NavigationMenu.Link>
+                </li>
+                <li style={{ gridRow: "span 3" }}>
+                  <NavigationMenu.Link asChild>
+                    <a className="Callout" href="/">
+                      <svg
+                        width="25"
+                        height="25"
+                        viewBox="0 0 15 15"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M12.1464 1.14645C12.3417 0.951184 12.6583 0.951184 12.8535 1.14645L14.8535 3.14645C15.0488 3.34171 15.0488 3.65829 14.8535 3.85355L10.9109 7.79618C10.8349 7.87218 10.7471 7.93543 10.651 7.9835L6.72359 9.94721C6.53109 10.0435 6.29861 10.0057 6.14643 9.85355C5.99425 9.70137 5.95652 9.46889 6.05277 9.27639L8.01648 5.34897C8.06455 5.25283 8.1278 5.16507 8.2038 5.08907L12.1464 1.14645ZM12.5 2.20711L8.91091 5.79618L7.87266 7.87267L8.12731 8.12732L10.2038 7.08907L13.7929 3.5L12.5 2.20711ZM9.99998 2L8.99998 3H4.9C4.47171 3 4.18056 3.00039 3.95552 3.01877C3.73631 3.03668 3.62421 3.06915 3.54601 3.10899C3.35785 3.20487 3.20487 3.35785 3.10899 3.54601C3.06915 3.62421 3.03669 3.73631 3.01878 3.95552C3.00039 4.18056 3 4.47171 3 4.9V11.1C3 11.5283 3.00039 11.8194 3.01878 12.0445C3.03669 12.2637 3.06915 12.3758 3.10899 12.454C3.20487 12.6422 3.35785 12.7951 3.54601 12.891C3.62421 12.9309 3.73631 12.9633 3.95552 12.9812C4.18056 12.9996 4.47171 13 4.9 13H11.1C11.5283 13 11.8194 12.9996 12.0445 12.9812C12.2637 12.9633 12.3758 12.9309 12.454 12.891C12.6422 12.7951 12.7951 12.6422 12.891 12.454C12.9309 12.3758 12.9633 12.2637 12.9812 12.0445C12.9996 11.8194 13 11.5283 13 11.1V6.99998L14 5.99998V11.1V11.1207C14 11.5231 14 11.8553 13.9779 12.1259C13.9549 12.407 13.9057 12.6653 13.782 12.908C13.5903 13.2843 13.2843 13.5903 12.908 13.782C12.6653 13.9057 12.407 13.9549 12.1259 13.9779C11.8553 14 11.5231 14 11.1207 14H11.1H4.9H4.87934C4.47686 14 4.14468 14 3.87409 13.9779C3.59304 13.9549 3.33469 13.9057 3.09202 13.782C2.7157 13.5903 2.40973 13.2843 2.21799 12.908C2.09434 12.6653 2.04506 12.407 2.0221 12.1259C1.99999 11.8553 1.99999 11.5231 2 11.1207V11.1206V11.1V4.9V4.87935V4.87932V4.87931C1.99999 4.47685 1.99999 4.14468 2.0221 3.87409C2.04506 3.59304 2.09434 3.33469 2.21799 3.09202C2.40973 2.71569 2.7157 2.40973 3.09202 2.21799C3.33469 2.09434 3.59304 2.04506 3.87409 2.0221C4.14468 1.99999 4.47685 1.99999 4.87932 2H4.87935H4.9H9.99998Z"
+                          fill="white"
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+
+                      <div className="CalloutHeading">
+                        Update Patient Profile
+                      </div>
+                    </a>
+                  </NavigationMenu.Link>
+                </li>
+              </ul>
+            </NavigationMenu.Content>
+          </NavigationMenu.Item>
+
+          <NavigationMenu.Item className="NavigationMenuItem">
+            <NavigationMenu.Trigger className="NavigationMenuTrigger">
+              Personnel <CaretDownIcon className="CaretDown" aria-hidden />
+            </NavigationMenu.Trigger>
+            <NavigationMenu.Content className="NavigationMenuContent">
+              <ul className="List two">
+                <a href="" className="ListItemLink">
+                  <p className="ListItemHeading">Add a Personnel</p>
+                </a>
+                <a href="" className="ListItemLink">
+                  <p className="ListItemHeading">Update a Personnel</p>
+                </a>
+                <a href="" className="ListItemLink">
+                  <p className="ListItemHeading">Terminate a Personnel</p>
+                </a>
+              </ul>
+            </NavigationMenu.Content>
+          </NavigationMenu.Item>
+
+          <NavigationMenu.Item className="NavigationMenuItem">
+            <NavigationMenu.Trigger className="NavigationMenuTrigger">
+              Service <CaretDownIcon className="CaretDown" aria-hidden />
+            </NavigationMenu.Trigger>
+            <NavigationMenu.Content className="NavigationMenuContent">
+              <ul className="List two">
+                <a href="" className="ListItemLink">
+                  <p className="ListItemHeading">Add a Service</p>
+                </a>
+                <a href="" className="ListItemLink">
+                  <p className="ListItemHeading">Update a Service</p>
+                </a>
+                <a href="" className="ListItemLink">
+                  <p className="ListItemHeading">Terminate a Service</p>
+                </a>
+              </ul>
+            </NavigationMenu.Content>
+          </NavigationMenu.Item>
+
+          <NavigationMenu.Item className="NavigationMenuItem">
+            <NavigationMenu.Trigger className="NavigationMenuTrigger">
+              Appointment <CaretDownIcon className="CaretDown" aria-hidden />
+            </NavigationMenu.Trigger>
+            <NavigationMenu.Content className="NavigationMenuContent">
+              <ul className="List two">
+                <a href="" className="ListItemLink">
+                  <p className="ListItemHeading">View Appointments</p>
+                </a>
+                <a href="" className="ListItemLink">
+                  <p className="ListItemHeading">Change an Appointment</p>
+                </a>
+                <a href="" className="ListItemLink">
+                  <p className="ListItemHeading">Cancel an Appointment</p>
+                </a>
+              </ul>
+            </NavigationMenu.Content>
+          </NavigationMenu.Item>
+
+          <NavigationMenu.Item className="NavigationMenuItem">
+            <NavigationMenu.Link
+              className="NavigationMenuLink"
+              href="https://github.com/radix-ui"
+            >
+              Queue
+            </NavigationMenu.Link>
+          </NavigationMenu.Item>
+
+          <NavigationMenu.Indicator className="NavigationMenuIndicator">
+            <div className="Arrow" />
+          </NavigationMenu.Indicator>
+        </NavigationMenu.List>
+
+        <div className="ViewportPosition">
+          <NavigationMenu.Viewport className="NavigationMenuViewport" />
+        </div>
+      </NavigationMenu.Root>
+    </div>
+  );
+};
+
+export default NavigationMenuDemo;
