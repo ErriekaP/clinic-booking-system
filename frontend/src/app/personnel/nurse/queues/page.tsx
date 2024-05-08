0e31f3e0e368aabf690f1bbed6520f46d0db1a38
Errieka Palanca <errieka.palanca@gmail.com>
2024-05-08 17:44:02 +0800

    fix: updated the stle for vitalSignsFormsDialog

--- frontend/src/app/personnel/nurse/queues/page.tsx
+++ frontend/src/app/personnel/nurse/queues/page.tsx
@@ -1,30 +1,27 @@
-"use client";
-import BackNavbar from "@/components/backNavbar/backNavbar";
-import FormsDialog from "@/components/formsDialog/page";
-import VitalSigns from "@/components/vitalSignsformsDialog/page";
-import dayjs from "dayjs";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+'use client';
+import BackNavbar from '@/components/backNavbar/backNavbar';
+import VitalSigns from '@/components/vitalSignsformsDialog/page';
+import dayjs from 'dayjs';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 //appointments under his number
 const Page = () => {
   const router = useRouter();
   const [queues, setQueues] = useState<any[]>([]);
-  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [searchQuery, setSearchQuery] = useState<string>('');
 
   useEffect(() => {
     // Fetch appointments data from an API
     const fetchAppointments = async () => {
       try {
-        const response = await fetch(
-          `${process.env.NEXT_PUBLIC_BACKEND_URL}/queue`
-        );
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/queue`);
         if (!response.ok) {
-          throw new Error("Failed to fetch appointments");
+          throw new Error('Failed to fetch appointments');
         }
         const data = await response.json();
         setQueues(data);
       } catch (error) {
-        console.error("Error fetching appointments:", error);
+        console.error('Error fetching appointments:', error);
       }
     };
 
@@ -35,7 +32,7 @@ const Page = () => {
 
   const handleClick = (queue: any) => {
     // Handle click action (if needed)
-    console.log("Clicked queue:", queue);
+    console.log('Clicked queue:', queue);
     router.push(`/personnel/nurse/queues/vitalSigns/${queue.id}`);
   };
 
@@ -44,20 +41,18 @@ const Page = () => {
     // Implement search functionality (if needed)
   };
 
-  const filteredQueues = queues.filter((queue) =>
-    queue.status.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  const filteredQueues = queues.filter((queue) => queue.status.toLowerCase().includes(searchQuery.toLowerCase()));
 
   const formatTime = (timeString: string) => {
     const hours = parseInt(timeString.slice(11, 13), 10);
     const minutes = timeString.slice(14, 16);
-    const period = hours < 12 ? "AM" : "PM";
+    const period = hours < 12 ? 'AM' : 'PM';
     const formattedHours = hours > 12 ? hours - 12 : hours;
     return `${formattedHours}:${minutes} ${period}`;
   };
   const formatDate = (dateString: string) => {
     const dayjsObject = dayjs(dateString);
-    const isoDateString = dayjsObject.format("MMMM D, YYYY	");
+    const isoDateString = dayjsObject.format('MMMM D, YYYY	');
     return isoDateString;
   };
 
@@ -65,93 +60,63 @@ const Page = () => {
     <div>
       <BackNavbar />
 
-      <div className="flex flex-col h-screen items-center">
-        <div className="block relative max-w-sm mb-4">
-          <span className="h-full absolute inset-y-0 left-0 flex items-center pl-2">
-            <svg
-              viewBox="0 0 24 24"
-              className="h-4 w-4 fill-current text-gray-500"
-            >
-              <path d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z"></path>
+      <div className='flex flex-col h-screen items-center'>
+        <div className='block relative max-w-sm mb-4'>
+          <span className='h-full absolute inset-y-0 left-0 flex items-center pl-2'>
+            <svg viewBox='0 0 24 24' className='h-4 w-4 fill-current text-gray-500'>
+              <path d='M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z'></path>
             </svg>
           </span>
           <input
-            placeholder="Search by status"
-            className="appearance-none rounded-md border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
+            placeholder='Search by status'
+            className='appearance-none rounded-md border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none'
             value={searchQuery}
             onChange={handleSearchChange}
           />
         </div>
-        <div className="flex flex-col flex-grow w-full max-w-4xl">
-          <div className="overflow-x-auto">
+        <div className='flex flex-col flex-grow w-full max-w-4xl'>
+          <div className='overflow-x-auto'>
             {/* Display the main appointments table */}
-            <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
-              <table className="table-fixed bg-white min-w-full leading-normal">
+            <div className='inline-block min-w-full shadow rounded-lg overflow-hidden'>
+              <table className='table-fixed bg-white min-w-full leading-normal'>
                 <thead>
                   <tr>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-sm font-bold uppercase">
-                      Queue ID
-                    </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-sm font-bold uppercase">
-                      Queue Name
-                    </th>
+                    <th className='px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-sm font-bold uppercase'>Queue ID</th>
+                    <th className='px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-sm font-bold uppercase'>Queue Name</th>
 
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-sm font-bold uppercase">
-                      Patient ID
-                    </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-sm font-bold uppercase">
-                      Patient Name
-                    </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-sm font-bold uppercase">
-                      Service
-                    </th>
+                    <th className='px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-sm font-bold uppercase'>Patient ID</th>
+                    <th className='px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-sm font-bold uppercase'>Patient Name</th>
+                    <th className='px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-sm font-bold uppercase'>Service</th>
 
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-sm font-bold uppercase">
-                      Status
-                    </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-sm font-bold uppercase">
-                      Forms
-                    </th>
+                    <th className='px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-sm font-bold uppercase'>Status</th>
+                    <th className='px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-sm font-bold uppercase'>Forms</th>
                   </tr>
                 </thead>
                 <tbody>
                   {filteredQueues.map((queue) => (
-                    <tr
-                      key={queue.id}
-                      className="hover:bg-gray-50 cursor-pointer"
-                    >
-                      <td className="px-5 py-4 border-b border-gray-200 bg-white text-sm text-center">
-                        {queue.id}
-                      </td>
-                      <td className="px-5 py-4 border-b border-gray-200 bg-white text-sm text-center">
-                        {queue.queueID}
-                      </td>
+                    <tr key={queue.id} className='hover:bg-gray-50 cursor-pointer'>
+                      <td className='px-5 py-4 border-b border-gray-200 bg-white text-sm text-center'>{queue.id}</td>
+                      <td className='px-5 py-4 border-b border-gray-200 bg-white text-sm text-center'>{queue.queueID}</td>
 
-                      <td className="px-5 py-4 border-b border-gray-200 bg-white text-sm text-center">
-                        {queue.patientID}
-                      </td>
+                      <td className='px-5 py-4 border-b border-gray-200 bg-white text-sm text-center'>{queue.patientID}</td>
 
-                      <td className="px-5 py-4 border-b border-gray-200 bg-white text-sm text-center">
-                        {" "}
+                      <td className='px-5 py-4 border-b border-gray-200 bg-white text-sm text-center'>
+                        {' '}
                         {queue.patient.firstName} {queue.patient.lastName}
                       </td>
 
-                      <td className="px-5 py-4 border-b border-gray-200 bg-white text-sm text-center">
-                        {queue.service.serviceName}
-                      </td>
+                      <td className='px-5 py-4 border-b border-gray-200 bg-white text-sm text-center'>{queue.service.serviceName}</td>
 
-                      <td className="px-5 py-4 border-b border-gray-200 bg-white text-sm text-center">
-                        {queue.status}
-                      </td>
-                      {queue.status === "ONGOING" ? (
+                      <td className='px-5 py-4 border-b border-gray-200 bg-white text-sm text-center'>{queue.status}</td>
+                      {queue.status === 'ONGOING' ? (
                         <>
-                          <td className="px-5 py-4 border-b border-gray-200 bg-white text-sm text-center">
+                          <td className='px-5 py-4 border-b border-gray-200 bg-white text-sm text-center'>
                             <VitalSigns queueId={queue.id} />
                           </td>
                         </>
                       ) : (
                         <>
-                          <td className="px-5 py-4 border-b border-gray-200 bg-white text-sm text-center"></td>
+                          <td className='px-5 py-4 border-b border-gray-200 bg-white text-sm text-center'></td>
                         </>
                       )}
                     </tr>
--- frontend/src/components/vitalSignsformsDialog/page.tsx
+++ frontend/src/components/vitalSignsformsDialog/page.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import * as Dialog from "@radix-ui/react-dialog";
-import { Cross2Icon } from "@radix-ui/react-icons";
-import "./styles.css";
-import { useRouter } from "next/navigation";
+import React, { useState } from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
+import { Cross2Icon } from '@radix-ui/react-icons';
+import './styles.css';
+import { useRouter } from 'next/navigation';
 
 const VitalSigns = ({ queueId }: { queueId: string }) => {
   const router = useRouter();
@@ -17,39 +17,36 @@ const VitalSigns = ({ queueId }: { queueId: string }) => {
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4  rounded focus:outline-none focus:shadow-outline ">
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4  rounded focus:outline-none focus:shadow-outline '>
           Vital Signs
         </button>
       </Dialog.Trigger>
       <Dialog.Portal>
-        <Dialog.Overlay className="DialogOverlay" />
-        <Dialog.Content className="DialogContent">
-          <Dialog.Title className="DialogTitle">Choose a Form</Dialog.Title>
-          <Dialog.Description className="DialogDescription"></Dialog.Description>
+        <Dialog.Overlay className='DialogOverlay' />
+        <Dialog.Content className='DialogContent'>
+          <Dialog.Title className='DialogTitle'>Choose a Form</Dialog.Title>
+          <Dialog.Description className='DialogDescription'></Dialog.Description>
 
           <div
             style={{
-              display: "flex",
+              display: 'flex',
               marginTop: 25,
-              justifyContent: "flex-end",
+              justifyContent: 'flex-end'
             }}
           >
             <Dialog.Close asChild>
-              <button className="Button blue" onClick={handlePhysicalExamClick}>
+              <button className='Button blue' onClick={handlePhysicalExamClick}>
                 Physical Examination
               </button>
             </Dialog.Close>
             <Dialog.Close asChild>
-              <button
-                className="Button green"
-                onClick={handleAfterCheckupClick}
-              >
+              <button className='Button green' onClick={handleAfterCheckupClick}>
                 After Check-up
               </button>
             </Dialog.Close>
           </div>
           <Dialog.Close asChild>
-            <button className="IconButton" aria-label="Close">
+            <button className='VitalSigns-IconButton' aria-label='Close'>
               <Cross2Icon />
             </button>
           </Dialog.Close>
